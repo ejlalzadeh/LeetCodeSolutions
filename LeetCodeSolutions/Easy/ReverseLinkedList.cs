bc890adc92043de34e376b9ce24037a6379c2b6c
Ali Ejlalzadeh <aliejlalzadeh1379@gmail.com>
2024-03-29 02:53:28 +0330

    Add ReverseLinkedList solution

---
+++ LeetCodeSolutions/Easy/ReverseLinkedList.cs
@@ -0,0 +1,33 @@
+﻿using LeetCodeSolutions.Medium;
+
+namespace LeetCodeSolutions.Easy;
+
+public class ReverseLinkedListSolution
+{
+    public static ListNode ReverseList(ListNode head)
+    {
+        if (head is null)
+            return null;
+
+        List<int> values = new();
+
+        while (head is not null)
+        {
+            values.Add(head.val);
+            head = head.next;
+        }
+
+
+        ListNode reversed = new();
+        ListNode currentNode = reversed;
+
+        for (int i = values.Count - 1; i >= 0; i--)
+        {
+            currentNode.val = values[i];
+            currentNode.next = i == 0 ? null : new ListNode();
+            currentNode = currentNode.next;
+        }
+
+        return reversed;
+    }
+}
--- LeetCodeSolutions/Program.cs
+++ LeetCodeSolutions/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using LeetCodeSolutions.Easy;
+using LeetCodeSolutions.Medium;
+
+ListNode l1 = new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, null))));
+ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4, null)));
+
+ListNode l3 = new ListNode(9, null);
+
+ListNode l4 = new ListNode(9,
+        new ListNode(9,
+            new ListNode(9,
+                new ListNode(9,
+                    new ListNode(9,
+                        new ListNode(9,
+                            new ListNode(9, null)))))));
+
+
+
+ReverseLinkedListSolution.ReverseList(l2);
